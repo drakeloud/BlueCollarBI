6a742c974de752b23152696ae760831995916154
Drake <louddrm@gmail.com>
2017-08-26 16:21:08 -0600

    deleted unused apps

--- bluecollarbi/blog/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-# from bluecollarbi.home.models import Home
-
--- bluecollarbi/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from bluecollarbi.blog import views
-
-urlpatterns = [
-    # url(r'^$', views.index, name='index'),
-    url(r'^index', views.index, name='index'),
-]
